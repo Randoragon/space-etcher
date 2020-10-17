3c3f754bbf3e40e66b843186705698e542147b5d
Randoragon <randoragongamedev@gmail.com>
2020-10-17 14:51:46 +0200

    Implement basic window, surface and game loop

--- space-etcher.c
+++ space-etcher.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include <RND_Game.h>
+#include <RND_ErrMsg.h>
 
 #include "space-etcher.h"
 
-int main()
+extern SDL_Window *window;
+extern SDL_Surface *surface;
+extern bool running;
+
+int gameloop()
 {
+    while (running) {
+        SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x00, 0x00));
+        SDL_UpdateWindowSurface(window);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    window = NULL;
+    surface = NULL;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        RND_ERROR("Failed to initialize SDL (%s)\n", SDL_GetError());
+        exit(1);
+    }
+    window = SDL_CreateWindow("Space Etcher",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        RND_ERROR("Failed to create window (%s)\n", SDL_GetError());
+        exit(1);
+    }
+    surface = SDL_GetWindowSurface(window);
+
+    running = true;
+    gameloop();
+
     return EXIT_SUCCESS;
 }
--- space-etcher.h
+++ space-etcher.h
@@ -1,4 +1,21 @@
 #ifndef SPACE_ETCHER_H
 #define SPACE_ETCHER_H
 
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+// Constants
+#define SCREEN_WIDTH 1366
+#define SCREEN_HEIGHT 768
+#define FPS 60
+
+// Functions
+int main(int argc, char *argv[0]);
+int gameloop();
+
+// Global variables
+SDL_Window *window;
+SDL_Surface *surface;
+bool running;
+
 #endif /* SPACE_ETCHER_H */
