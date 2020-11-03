d1cbca2d12c356faaa8a388d0f6ccddac19286f3
Randoragon <randoragongamedev@gmail.com>
2020-11-03 13:02:49 +0100

    Add comment block for objs/gen-include.sh

--- objs/gen-include.sh
+++ objs/gen-include.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/sh
+#
+# Auto-generates the objs/include.h file, which is then included
+# by space-etcher.c. This is a big commodity, as the number of
+# objects in the project will rise inevitably, and each object
+# needs its own header and source files, which then need to be
+# included by the main program.
 
 dir="$(dirname -- "$0")"
 
